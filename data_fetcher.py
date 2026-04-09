7d6fc9a74eacd0c20db1a74bb0ca1fd8e3843d0a
ryomenhaider <onlinep680@gmail.com>
2026-04-09 16:43:24 +0500

    project started

--- data_fetcher.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-
-def fetch_orderboook(symbol:str, limit:int = 20):
-    
-    res = requests.get(f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={limit}')
-    
--- enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class book(Enum):
-    BID = 'bid'
-    ASK = 'ask'
-    ORDERBOOK = 'orderbook'
